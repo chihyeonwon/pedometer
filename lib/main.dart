1bc07ef2f29ae7e42818064a61770cb1d6600e91
ylose <heungeob1003@gmail.com>
2023-02-23 13:58:31 +0900

    앱 바 설정

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
+    return const MaterialApp(
+      title: 'walkinghelath',
       home: HomeScreen(),
     );
   }
@@ -31,9 +28,39 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: Text('홈',),
+      appBar: AppBar(
+        elevation: 0.0, // 양각 제거 (그림자)
+        toolbarHeight:50.0, // 앱 바 높이 설정
+        backgroundColor: Colors.yellow[600], // 앱 바 색상
+        title: const Text(
+          '홈',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Padding(
+              padding: EdgeInsets.only(left:10.0,),
+              child: Icon(
+                Icons.notifications,
+                color: Colors.black,
+                size: 30.0,
+              ),
+            ),
+            onPressed: () {}, // 잠금 화면으로 이동
+          ),
+          IconButton(
+            icon: const Icon(
+                Icons.screen_lock_portrait,
+                color: Colors.black,
+              size: 30.0,
+            ),
+            onPressed: () {}, // 잠금 화면으로 이동
+          ),
+        ],
       ),
     );
   }
