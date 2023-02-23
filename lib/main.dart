48cee484385a4189b8c06e64e6c6bd19d146d3de
ylose <heungeob1003@gmail.com>
2023-02-23 14:49:35 +0900

    TabController length 수정(3->4)

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 3, vsync: this);
+    tabController = TabController(length: 4, vsync: this);
   }
 
   @override
