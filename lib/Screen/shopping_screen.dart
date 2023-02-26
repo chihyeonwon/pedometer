7b236adc49db605f81570eb792f356220e95484e
ylose <heungeob1003@gmail.com>
2023-02-26 12:47:45 +0900

    Top, Middle, Bottom으로 화면 분리

--- lib/Screen/shopping_screen.dart
+++ lib/Screen/shopping_screen.dart
@@ -5,6 +5,43 @@ class ShoppingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: const [
+          Top(),
+          Middle(),
+          Bottom(),
+        ],
+      ),
+    ));
   }
 }
+
+class Top extends StatelessWidget {
+  const Top({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Middle extends StatelessWidget {
+  const Middle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Bottom extends StatelessWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
