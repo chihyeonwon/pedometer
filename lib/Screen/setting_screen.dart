3bdc99bc29485a74ec41c916e2796b0c497b4a80
ylose <heungeob1003@gmail.com>
2023-03-01 02:21:00 +0900

    SettingScreen Middle Rendering Overflow 문제 해결

--- lib/Screen/setting_screen.dart
+++ lib/Screen/setting_screen.dart
@@ -112,7 +112,6 @@ class _MiddleState extends State<Middle> {
   Widget build(BuildContext context) {
     return Material(
       child: Container(
-        height: MediaQuery.of(context).size.height,
         child: Column(
           children: [
             Stack(
@@ -467,6 +466,53 @@ class _MiddleState extends State<Middle> {
                               _isNews = value;
                             });
                           },
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 30.0,
+                  color: Colors.grey[300],
+                ),
+                Positioned(
+                  left: 20.0,
+                  top: 4.0,
+                  child: Text('알림',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ),
+              ],
+            ),
+            InkWell(
+              onTap: () {}, // 캐시웨어 페이지로 이동
+              child: Ink(
+                  height: 65.0,
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                            color: Colors.grey,
+                          ))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Text('캐시웨어',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 25.0,
                         ),
                       ),
                     ],
