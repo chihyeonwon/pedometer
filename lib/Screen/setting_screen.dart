739ff05b0719d5419b33a6d8241eb368a9b16d6d
ylose <heungeob1003@gmail.com>
2023-03-01 01:10:23 +0900

    SettingScreen Top 위젯 구현

--- lib/Screen/setting_screen.dart
+++ lib/Screen/setting_screen.dart
@@ -5,6 +5,96 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: const [
+          Top(),
+          Middle(),
+        ],
+      ),
+    ));
+  }
+}
+
+class Top extends StatelessWidget {
+  const Top({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.yellow[600],
+                border: Border.all(
+                  width: 4.0,
+                  color: Colors.black,
+                ),
+              ),
+              width: 60.0,
+              height: 60.0,
+              child: Icon(Icons.shopping_cart_sharp, size: 50.0),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Text('현재 버전',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('1.23.38',
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text('최신 버전',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('1.23.38',
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:55.0),
+            child: Text('최신 버전 사용 중', style: TextStyle(fontWeight:FontWeight.bold, fontSize:15.0,)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  const Middle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
